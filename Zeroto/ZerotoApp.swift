f2226dbc42ebbfcc7286f0c3f55457075d38e36f
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-02-16 23:15:16 +0000

    Initial Commit

---
+++ Zeroto/ZerotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZerotoApp.swift
+//  Zeroto
+//
+//  Created by user213454 on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ZerotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
